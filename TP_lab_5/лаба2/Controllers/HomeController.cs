7cf5014373e99db4b1986187250f6bfa046c1be6
DaryaGracheva <99476603+DaryaGracheva@users.noreply.github.com>
2023-05-17 21:32:51 +0400

    Добавьте файлы проекта.

---
+++ TP_lab_5/лаба2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using лаба2.Models;
+
+namespace лаба2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string id, DateTime data, string adress_sender, string adress_recipient, string weight, string price, string delivery_method)
+        {
+            string authData = $"Посылка оформлена!  " +
+                $"ID: {id}  Дата заказа: {data}  Откуда: {adress_sender} Куда: {adress_recipient} Вес посылки (кг): {weight} Ценность посылки (руб.): {price}  Способ доставки: {Convert.ToString(delivery_method)} ";
+            return Content(authData);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TP_lab_5/лаба2/Models/dataset.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace лаба2.Models
+{
+    [DisplayName("Оформить посылку")]
+    public class dataset
+    {
+        public string? id { get; set; }
+
+        [DisplayName("Дата")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? data { get; set; }
+
+        [DisplayName("Откуда")]
+        [DataType(DataType.MultilineText)]
+        public string? adress_sender { get; set; }
+
+        [DisplayName("Куда")]
+        [DataType(DataType.MultilineText)]
+        public string? adress_recipient { get; set; }
+
+        [DisplayName("Вес посылки")]
+        public string? weight { get; set; }
+
+        [DisplayName("Ценность посылки")]
+        public string? price { get; set; }
+
+        [DisplayName("Способ доставки")]
+        public bool delivery_method { get; set; }
+
+    }
+
+    public enum delivery_method
+    {
+        regular,
+        expedited,
+        by_courier
+
+    }
+}
